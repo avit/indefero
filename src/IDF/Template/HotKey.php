d19dbb441c86740d6a2df2501cb5875a5fc34e19
Loic d'Anterroches <titoo@users.sourceforge.net>
2008-07-25 11:16:18 +0200

    Added a hotkey tag to easily add them.
    Added a new Shift+m hotkey to access the list of submitted issues.

---
+++ src/IDF/Template/HotKey.php
@@ -0,0 +1,36 @@
+<?php
+/* -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+# ***** BEGIN LICENSE BLOCK *****
+# This file is part of InDefero, an open source project management application.
+# Copyright (C) 2008 Céondo Ltd and contributors.
+#
+# InDefero is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# InDefero is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# ***** END LICENSE BLOCK ***** */
+
+Pluf::loadFunction('Pluf_HTTP_URL_urlForView');
+
+/**
+ * Hotkey tag.
+ */
+class IDF_Template_HotKey extends Pluf_Template_Tag
+{
+    function start($key, $view, $params=array(), $get_params=array())
+    {
+        $url = addslashes(Pluf_HTTP_URL_urlForView($view, $params, $get_params));
+        echo "jQuery.hotkeys.add('$key',function (){window.location.href='$url';});";
+    }
+}
