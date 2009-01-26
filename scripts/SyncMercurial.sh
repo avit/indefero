c11cd7bc6260ad5da6f6070be1608b1f172a784a
Loic d'Anterroches <titoo@users.sourceforge.net>
2009-01-26 09:44:32 +0100

    Fixed issue 110, SyncMercurial error and undocumented requirement.
    Added the documentation.

--- scripts/SyncMercurial.sh
+++ scripts/SyncMercurial.sh
@@ -4,7 +4,7 @@ private_notify="/home/indefero/tmp/notify.tmp"
 reload_cmd="/usr/sbin/apachectl -k graceful"
 
 if [ -e $private_notify ]; then
-    $reload_cmd
     rm -f $private_notify
+    $reload_cmd
 fi
   
