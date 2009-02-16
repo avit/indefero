9cd2d1c4ef627cdad28f1336c1a3618506bcf290
Loic d'Anterroches <titoo@users.sourceforge.net>
2009-02-16 11:55:14 +0100

    Added the relations of the IDF_Commit model.

--- src/IDF/relations.php
+++ src/IDF/relations.php
@@ -39,6 +39,7 @@ $m['IDF_Review_Patch'] = array('relate_to' => array('IDF_Review', 'Pluf_User'));
 $m['IDF_Review_FileComment'] = array('relate_to' => array('IDF_Review_Patch', 'Pluf_User'));
 $m['IDF_Key'] = array('relate_to' => array('Pluf_User'));
 $m['IDF_Conf'] = array('relate_to' => array('IDF_Project'));
+$m['IDF_Commit'] = array('relate_to' => array('IDF_Project', 'Pluf_User'));
 
 Pluf_Signal::connect('Pluf_Template_Compiler::construct_template_tags_modifiers',
                      array('IDF_Middleware', 'updateTemplateTagsModifiers'));
